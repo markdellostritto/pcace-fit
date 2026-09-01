85be809b398237a51f8afc7b61921a93c58bc744
markdellostritto <43013452+markdellostritto@users.noreply.github.com>
2026-09-01 12:07:12 -0400

    Improved thermostat

--- Ar/fit_sr_pauli/md-pcace-cuda.py
+++ Ar/fit_sr_pauli/md-pcace-cuda.py
@@ -5,29 +5,19 @@
 # ==== global ====
 
 import sys
-import os
-import pickle
-import numpy as np
 import torch
-#import torch.nn as nn
 torch.set_default_dtype(torch.float64)
 
 # ==== pcace ====
 
-import pcace
 from pcace.calculators import CACECalculator
-from pcace.mlp import CACE
-from pcace.mlp import ANN_SR
-from pcace.mlp import NNP
 
 # ==== ase ====
 
+import ase
 from ase import units
-from ase.md.langevin import Langevin
-from ase.md.npt import NPT
-from ase.md.nptberendsen import NPTBerendsen
 from ase.md import MDLogger
-from ase.io import read, write
+from ase.io import read
 
 # ==== md ====
 n_run   = 40000
@@ -36,7 +26,7 @@ n_print = 100
 n_write = 100
 n_steps = int(n_run/n_print)
 
-print("md parameters:")
+print( "md parameters:")
 print(f"n_run   = {n_run}")
 print(f"n_log   = {n_log}")
 print(f"n_print = {n_print}")
@@ -85,29 +75,40 @@ calculator = CACECalculator(
     )
 conf_init.calc=calculator
 
-# Set initial velocities using Maxwell-Boltzmann distribution
+# set initial velocities 
 print("setting initial velocities")
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 temperature = float(sys.argv[2])# in K
-MaxwellBoltzmannDistribution(
-        atoms = conf_init, 
-        temperature_K = temperature * units.kB
+# set temperature
+ase.md.velocitydistribution.thermalize_momenta(
+    atoms = conf_init,
+    temperature_K = temperature,
+)
+# remove COM momentum
+ase.md.velocitydistribution.Stationary(
+    atoms = conf_init,
+    preserve_temperature = True,
+)
+# remove net rotation
+ase.md.velocitydistribution.ZeroRotation(
+    atoms = conf_init,
+    preserve_temperature = True,
 )
 
 # Define the NPT ensemble
 print("defining npt ensemble")
 ts = 1.0 * units.fs # timestep
-tscale = 25.0 * units.fs # time constant for temperature
-pscale = 75.0 * units.fs # time constant for pressure
-B = 2.0 * units.GPa # bulk modulus
-dyn = NPT(
-        conf_init,
-        timestep = ts, 
-        temperature_K = temperature,
-        ttime = tscale, 
-        pfactor = B*pscale**2,
-        externalstress = 0.0
-    )
+dyn = ase.md.nose_hoover_chain.MTKNPT(
+    atoms = conf_init,
+    timestep = ts,
+    temperature_K = temperature,
+    pressure_au = 0.0 * units.GPa,
+    tdamp =  500.0 * ts,
+    pdamp = 5000.0 * ts,
+    tchain = 3,
+    pchain = 3,
+    tloop = 1,
+    ploop = 1,
+)
 dyn.attach(
         write_frame, 
         interval=n_write
